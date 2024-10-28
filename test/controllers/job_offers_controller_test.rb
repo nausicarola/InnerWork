b54028a76b581d1ba62ecb2e9c04bb96d264b63b
nausicarola <carola.pulgar.soto@gmail.com>
2024-10-28 16:55:12 -0300

    Test

--- test/controllers/job_offers_controller_test.rb
+++ test/controllers/job_offers_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class JobOffersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+  end
+  test "should get show" do
+    get job_offers_path
+    assert_response :success
+  end
+  test "should get new" do
+  end
+  test "should get edit" do
+  end
+  test "should create a post" do
+  end
+  test "should update a post" do
+  end
+  test "should destroy a post" do
+  end
 end
